01f65f3dfed6acff97be84e4b2dc8a345b9cf4f7
kento.hayashinaka <clark0821kent@gmail.com>
2020-11-02 08:40:36 +0000

    badge

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     @fav_microposts = @user.fav_microposts.page(params[:page])
+    counts(@user)
   end
   
   private
